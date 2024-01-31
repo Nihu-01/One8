7f0e7aca51cc59de094c873deb49a237126f1610
Niharika Sable <niharikasable@gmail.com>
2024-01-31 18:19:45 +0530

    push folder

---
+++ one8website/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import background_img from './assets/images/background_img_V.png';
+import logo-yellow_V from './assets/images/logo-yellow_V.png';
+import one8-logo_V from './assets/images/one8-logo_V.png';
+import background_img from './assets/images/background_img_V.png';
+import background_img from './assets/images/background_img_V.png';
+
+
+
+const App = () => {
+  return (
+    <div className='logo'>
+      
+    </div>
+  )
+}
+
+export default App
+
